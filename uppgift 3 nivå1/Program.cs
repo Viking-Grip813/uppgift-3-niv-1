6f4971746a1dcdc326627dc7b827000fbb1d0091
vist07001 <vist07001@E-PW05CGR8>
2024-12-11 13:15:28 +0100

    Add project files.

---
+++ uppgift 3 nivå1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace uppgift3
+{ class Program
+    { static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv fem heltal per rad");
+            int tal = int .Parse(Console.ReadLine());
+            int tal1 = int.Parse(Console.ReadLine());
+            int tal2 = int.Parse(Console.ReadLine());
+            int tal3 = int.Parse(Console.ReadLine());
+            int tal4 = int.Parse(Console.ReadLine());
+            Console.WriteLine(tal + tal1 + tal2 + tal3 + tal4);
+
+        }
+    }
+}
